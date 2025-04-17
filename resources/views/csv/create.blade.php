6e50a14d77c2e494078eb4450e96939ef7fec96d
Juanjo <juanjotr31@gmail.com>
2025-04-17 19:52:04 +0200

    fix: bootstrap

--- resources/views/csv/create.blade.php
+++ resources/views/csv/create.blade.php
@@ -4,28 +4,30 @@
 
 @section('contenido')
     <h1 class="text-center">Subir Archivos</h1>
-    <form action="{{ route('csv.store') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-        <div class="mb-3">
-            <label for="dni" class="form-label">DNI: </label>
-            <input type="text" class="form-control" id="dni" name="dni" value="{{old('dni')}}" required>
-        </div>
-        <div class="mb-3">
-            <label for="nombre" class="form-label">Nombre: </label>
-            <input type="text" class="form-control" id="nombre" name="nombre" value="{{old('nombre')}}" required>
-        </div>
-        <div class="mb-3">
-            <label for="apellidos" class="form-label">Apellidos: </label>
-            <input type="text" class="form-control" id="apellidos" name="apellidos" value="{{old('apellidos')}}">
-        </div>
-        <div class="mb-3">
-            <label for="correo" class="form-label">Correo electronico: </label>
-            <input type="text" class="form-control" id="correo" name="correo" value="{{old('correo')}}" required>
-        </div>
-        <div class="mb-3">
-            <label for="archivo" class="form-label">Archivo: </label>
-            <input type="file" class="form-control" id="archivo" name="archivo" accept="application/pdf" required>
-        </div>
-        <button type="submit">Enviar</button>
-    </form>
+    <div class="container my-5 mx-auto">
+        <form action="{{ route('csv.store') }}" method="POST" enctype="multipart/form-data" class="row g-3 mt-3">
+        @csrf
+            <div class="col-md-6">
+                <label for="dni" class="form-label">DNI: </label>
+                <input type="text" class="form-control" id="dni" name="dni" value="{{old('dni')}}" required>
+            </div>
+            <div class="col-md-6">
+                <label for="nombre" class="form-label">Nombre: </label>
+                <input type="text" class="form-control" id="nombre" name="nombre" value="{{old('nombre')}}" required>
+            </div>
+            <div class="col-md-6">
+                <label for="apellidos" class="form-label">Apellidos: </label>
+                <input type="text" class="form-control" id="apellidos" name="apellidos" value="{{old('apellidos')}}">
+            </div>
+            <div class="col-md-6">
+                <label for="correo" class="form-label">Correo electronico: </label>
+                <input type="text" class="form-control" id="correo" name="correo" value="{{old('correo')}}" required>
+            </div>
+            <div class="col-md-6">
+                <label for="archivo" class="form-label">Archivo: </label>
+                <input type="file" class="form-control" id="archivo" name="archivo" accept="application/pdf" required>
+            </div>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
 @endsection
